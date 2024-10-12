db2b9c7f168897541116708288c501a0ef8b0db8
Syeda Nabiha <159924237+Syeda-Nabihaa@users.noreply.github.com>
2024-10-12 19:28:34 +0500

    3rd commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './module/header/header.component';
+// import { HeaderComponent } from './module/header/header.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { CalculatorComponent } from './module/calculator/calculator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    CalculatorComponent
   ],
   imports: [
     BrowserModule,
